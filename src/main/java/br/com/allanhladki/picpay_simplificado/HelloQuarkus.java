f245ca30d4ca23ef88c08ced852c0dae3b76705a
allanhladki <allanhladki.ah@gmail.com>
2023-12-04 18:11:39 -0300

    configuracao inicial do projeto

---
+++ src/main/java/br/com/allanhladki/picpay_simplificado/HelloQuarkus.java
@@ -0,0 +1,17 @@
+package br.com.allanhladki.picpay_simplificado;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloQuarkus {
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Configuração inicial do projeto";
+    }
+
+}
